c577e315efc9b174f1692bceab4c1dce36ddcd4d
Shawn Leo <xt-liu18@mails.tsinghua.edu.cn>
2020-07-19 15:36:10 +0800

    添加了注释

--- OurStory/ViewController.swift
+++ OurStory/ViewController.swift
@@ -20,14 +20,25 @@ class ViewController: UIViewController, WebSocketDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         //https://echo.websocket.org
-        var request = URLRequest(url: URL(string: "ws://192.168.31.88:4396/")!) //https://localhost:8080
+        var request = URLRequest(url: URL(string: "ws://192.168.31.88:4396/")!) //此为服务器的ip和端口信息，目前暂时以魔法值存在
         request.timeoutInterval = 5
-        socket = WebSocket(request: request)
+        socket = WebSocket(request: request) //创建了一个socket变量
         socket.delegate = self
-        socket.connect()
+        socket.connect()  //连接到服务器
     }
 
     // MARK: - WebSocketDelegate
+    /**************************************************************************************
+     .
+        函数名称：didReceive
+        函数功能：描述客户端收到各类信息时的处理方式
+        参数接口：使用协议默认参数，我们无需修改
+        返回值：无
+        例子：当客户端连接到服务器时，收到一个.connected信息，触发相关case，向控制台输出"websocket is connected:             \(headers)"，向服务器发送“Hi Server"
+        更改日志：2020/07/16 由刘相廷创建，并添加连接成功时向服务器发信的功能
+        备注：此函数参考了starscream的自带demo
+     .
+     **************************************************************************************/
     func didReceive(event: WebSocketEvent, client: WebSocket) {
         switch event {
         case .connected(let headers):
@@ -57,6 +68,7 @@ class ViewController: UIViewController, WebSocketDelegate {
         }
     }
     
+    //错误处理函数，直接拷贝了starscream中demo的写法，不必修改
     func handleError(_ error: Error?) {
         if let e = error as? WSError {
             print("websocket encountered an error: \(e.message)")
@@ -67,10 +79,10 @@ class ViewController: UIViewController, WebSocketDelegate {
         }
     }
     
+    //析构函数
     deinit {
       socket.disconnect()
       socket.delegate = nil
     }
 
 }
-
