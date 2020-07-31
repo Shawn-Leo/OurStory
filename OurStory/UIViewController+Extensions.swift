ef7a98bb9727d74778ee740e266c0b73e18b8aa3
Junpeng Lin <915323728@qq.com>
2020-07-31 23:30:04 +0800

    Design the login and sign up screen for Ourstory and add some extensions of UIViewcontroll

---
+++ OurStory/UIViewController+Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  UIViewController+Extensions.swift
+//  OurStory
+//
+//  Created by Momo on 2020/7/31.
+//  Copyright © 2020 Tsinghua ios Club. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+        // nothing goes here
+    }
+}
